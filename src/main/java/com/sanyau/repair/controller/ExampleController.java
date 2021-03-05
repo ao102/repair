ce2d65ce6b84df76723d125a8eb9418ebac9368c
wildsky <1399226280@qq.com>
2021-03-05 15:19:42 +0800

    增加分页查询案例

--- src/main/java/com/sanyau/repair/controller/ExampleController.java
+++ src/main/java/com/sanyau/repair/controller/ExampleController.java
@@ -1,13 +1,16 @@
 package com.sanyau.repair.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sanyau.repair.entity.StudentInfo;
 import com.sanyau.repair.response.Result;
 import com.sanyau.repair.service.impl.StudentInfoServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -39,6 +42,25 @@ public class ExampleController {
         /* 偷懒不写验证了 */
         boolean save = studentInfoService.save(user);
         return save ? Result.ok().data("msg", "suc") : Result.error();
-//        return Result.ok().data("info",info);
+    }
+
+    /* 分页查询返回结果就按这个格式写 */
+    @ApiOperation("分页查询")
+    @GetMapping("pageStudent")
+    public Result examplePage(@ApiParam(name = "current", value = "当前页", required = true) @RequestParam("current") Long current,
+                              @ApiParam(name = "limit", value = "每页数据量", required = true) @RequestParam("limit") Long limit){
+        //创建page对象
+        /* current: 当前页  size:每页显示size条数据*/
+        Page<StudentInfo> page = new Page<>(current,limit);
+        //调用方法实现分页
+        //调用方法的时候，底层做了封装，把分页所有数据封装到page对象里面
+        //queryWrapper为查询条件，填null即为全部查找
+        studentInfoService.page(page,null);
+        long total = page.getTotal();//总记录数
+        List<StudentInfo> records = page.getRecords();//数据list集合
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("total", total);
+        map.put("rows", records);
+        return Result.ok().data(map);
     }
 }
